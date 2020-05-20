9dd786b60cd6411e392fa3490731b20fed16c5a6
Muskan Bhandari <bmuskan007@gmail.com>
2020-05-20 10:44:01 +0545

    Added the styling to our homepage

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../directory-items/directory-items.component';
+export const HomePage = ()=>{
+    return(
+        <div className ='homepage' >
+            <Directory/>             
+        </div>
+    );
+}
+export default HomePage;
